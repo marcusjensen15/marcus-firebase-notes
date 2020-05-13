4e78596b0ef5e2bb5578f3859798e3379782ad8d
Marcus Jensen <marcusjensen1515@gmail.com>
2020-05-13 14:26:21 -0700

    add update body function to editor.js

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -7,6 +7,8 @@ import styles from './styles';
 
 //The export default withStyles(styles)(EditorComponent) is styling our EditorComponent. Its going to be a shortcut for styling
 
+//Debouncing is writing a seperate function to handle updating a database when a user stops typing. The purpose of this is so the app isn't making a ton of HTTP requests. 
+
 
 
 class EditorComponent extends React.Component {
@@ -25,10 +27,22 @@ class EditorComponent extends React.Component {
 
     return(
       <div className={classes.editorContainer}>
-        <ReactQuill></ReactQuill>
+        <ReactQuill
+          value={this.state.text}
+          onChange={this.updateBody}>
+        </ReactQuill>
       </div>
     );
   }
+
+  updateBody = async (val) => {
+    await this.setState({text: val});
+    this.update();
+  };
+
+  update = debounce(() => {
+
+  });
 }
 
 export default withStyles(styles)(EditorComponent);
