64831622854a6242a83db3d9bf2177a1621013d4
Marcus Jensen <marcusjensen1515@gmail.com>
2020-05-13 11:55:47 -0700

    add more notes to app.js firebase functions

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ const firebase = require('firebase');
 //componentDidMount - executing all of the code within the function as the component mounts.
 //firebase notes: collection is basically a table within a database.
 //onSnapshot - automatically gets called every time the 'notes' collection is updated inside of firebase. The paramater (function) within on snapShot is what is called everytime 'notes' updates
+//serverUpdate is what we are calling the parameter passed into onSnapshot. The serverUpdate object has a docs property.
+// data() is a function that grabs the data from the doc.
+//data['id'] -> adding the id property to our data object.
 
 class App extends React.Component {
 
@@ -25,7 +28,15 @@ class App extends React.Component {
   }
 
   componentDidMount =() => {
-    firebase.firestore().collection('notes').onSnapshot();
+    firebase.firestore().collection('notes').onSnapshot(serverUpdate => {
+      const notes = serverUpdate.docs.map(_doc => {
+        const data = _doc.data();
+        data['id'] = _doc.id;
+        return data;
+      });
+      console.log(notes);
+      this.setState({ notes: notes});
+    });
   }
 
 
