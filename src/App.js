6ccba9d531ba04592082447caa7dcac677cbc56f
Marcus Jensen <marcusjensen1515@gmail.com>
2020-05-13 11:45:50 -0700

    add firebase functions within componentDidMount inside of app.js. add notes explaining what is happening on the firebase side inside of app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './App.css';
 
+const firebase = require('firebase');
+
+//componentDidMount - executing all of the code within the function as the component mounts.
+//firebase notes: collection is basically a table within a database.
+//onSnapshot - automatically gets called every time the 'notes' collection is updated inside of firebase. The paramater (function) within on snapShot is what is called everytime 'notes' updates
+
 class App extends React.Component {
 
   constructor(){
@@ -15,10 +21,14 @@ class App extends React.Component {
 
   render(){
     return(
-      <div> Howdy.</div>
-
-    );
+      <div> Howdy.</div>);
   }
+
+  componentDidMount =() => {
+    firebase.firestore().collection('notes').onSnapshot();
+  }
+
+
 }
 
 export default App;
