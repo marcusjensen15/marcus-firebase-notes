12da3dce4f94f9fe1d847ff5008dbc81c919552e
Marcus Jensen <marcusjensen1515@gmail.com>
2020-05-13 11:35:07 -0700

    add initial 3 state slices in app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      selectedNoteIndex: null,
+      selectedNote: null,
+      notes: null
+
+    };
+  }
 
   render(){
     return(
