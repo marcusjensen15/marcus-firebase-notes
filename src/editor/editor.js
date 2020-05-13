6010d852c0758cf405a1f07795fbadd938bec652
Marcus Jensen <marcusjensen1515@gmail.com>
2020-05-13 14:35:06 -0700

    debounce function is sucessfully working. logs update db after 1.5 seconds of inactivity.

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -9,6 +9,9 @@ import styles from './styles';
 
 //Debouncing is writing a seperate function to handle updating a database when a user stops typing. The purpose of this is so the app isn't making a ton of HTTP requests. We are passing our update function into our debounce function.
 
+//updateBody is calling update which is calling debounce. 1500 is being passed into debounce as a parameter 'b' in helpers.js. Basically, everytime you type something it restarts that 1.5 second timer. We could adjust the 1.5 to any length of timeout before making another http request.
+
+
 
 
 class EditorComponent extends React.Component {
