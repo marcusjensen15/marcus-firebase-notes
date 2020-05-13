913b403fc0a99860d67ab00d58cf2b1ccaaac23b
Marcus Jensen <marcusjensen1515@gmail.com>
2020-05-13 12:35:41 -0700

    sidebar.js code now in place

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -4,3 +4,22 @@ import debounce from '../helpers';
 import BorderColorIcon from '@material-ui/icons/BorderColor';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
+
+//The export default withStyles(styles)(EditorComponent) is styling our EditorComponent. Its going to be a shortcut for styling
+
+
+
+class EditorComponent extends React.Component {
+
+  constructor() {
+    super();
+  }
+
+  render(){
+    return(
+      <div> This is the Editor Page </div>
+    );
+  }
+}
+
+export default withStyles(styles)(EditorComponent);
