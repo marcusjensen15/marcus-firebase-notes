f5201252d28a7be4ab7e3bd7d17682d30680f360
Marcus Jensen <marcusjensen1515@gmail.com>
2020-05-14 11:48:58 -0700

    add delete note icon, about to begin delete note function

--- src/sidebaritem/sidebaritem.js
+++ src/sidebaritem/sidebaritem.js
@@ -25,11 +25,12 @@ class SidebarItemComponent extends React.Component {
             <ListItemText
               primary={_note.title}
               secondary={removeHTMLTags(_note.body.substring(0, 30)) + '...'}>
-
-
             </ListItemText>
           </div>
-
+          <DeleteIcon
+            onClick={() => this.deleteNote(_note)}
+            className={classes.deleteIcon}
+            ></DeleteIcon>
 
         </ListItem>
 
